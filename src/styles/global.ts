c5fe43e80dd1530ebad144742b5854f6cc3b26a2
UL xD <whelidias@hotmail.com>
2022-02-14 12:16:33 -0300

    Modal + styles created

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,6 +7,7 @@ export const GlobalStyle = createGlobalStyle`
         --background: #f0f2f5;
         --red: #e52e4d;
         --blue: #5429cc;
+        --green: #33cc95;
         --blue-light: #6933ff;
         --text-title: #363f5f; 
         --text-body: #969cb3;
@@ -34,6 +35,15 @@ export const GlobalStyle = createGlobalStyle`
         -webkit-font-smoothing: antialiased;
     }
 
+    body,input,textarea,button{
+        font-family: 'Poppin', sans-serif;
+        font-weight: 400;
+    }
+
+    h1,h2,h3,h4,h5,h6,strong{
+        font-weight: 600;
+    }
+
     button{
         cursor: pointer;
     }
@@ -41,5 +51,44 @@ export const GlobalStyle = createGlobalStyle`
     [disabled]{
         opacity: 0.6;
         cursor: not-allowed;
+    }
+
+    
+
+    .rect-modal-overlay{
+        background: rgba(0,0,0,0.5);
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    
+    .react-modal-content{
+        width: 100%;
+        max-width: 576px;
+        background: var(--background);
+        position: relative;
+        padding: 3rem;
+        border-radius: 0.24rem;
+    }
+
+    .react-modal-close{
+        position: absolute;
+        right: 1.5rem;
+        top: 1.5rem; 
+
+        border: 0;
+        background: transparent;
+
+        transition: filter 0.3s;
+
+        &:hover {
+            filter: brightness(0.8);
+        }
     }
 `
